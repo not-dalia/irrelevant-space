f2feb9a793fac87519bd3247503f2208dafe4334
Dalia <dalia.alshahrabi@gmail.com>
2024-01-09 12:24:42 +0000

    test git dates

--- _plugins/obsidian_meadow.rb
+++ _plugins/obsidian_meadow.rb
@@ -76,12 +76,13 @@ module Jekyll
 
       git_safe_directory_command = `git config --global --add safe.directory /github/workspace`
       # Jekyll.logger.info "git config --global --add safe.directory /github/workspace", git_safe_directory_command
-      git_command_last_modified = `git log --follow -1 --format=%cd --date=iso-strict -- "#{doc.path}"`
       git_full_log_command = `git log  --format=%ad --date=iso-strict -- "#{doc.path}"`
-      git_command_created_at = `git log --follow --format=%ad --date=iso-strict -- "#{doc.path}" | tail -1`
+      git_date_array = git_full_log_command.split("\n")
+      git_last_modified = git_date_array[0]
+      git_created_at = git_date_array[-1]
       Jekyll.logger.info "file: #{doc.path}", git_full_log_command
-      Jekyll.logger.info "file: #{doc.path}", git_command_last_modified
-      Jekyll.logger.info "file: #{doc.path}", git_command_created_at
+      Jekyll.logger.info "file: #{doc.path}", git_last_modified
+      Jekyll.logger.info "file: #{doc.path}", git_created_at
     end
 
     Jekyll::Hooks.register :site, :post_render do |site|
